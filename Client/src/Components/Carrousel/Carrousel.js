c96dbbf8af5917f0710a359c1bd35d12f37b3ed8
AlannV <97909677+AlannV@users.noreply.github.com>
2022-08-26 11:26:12 -0300

    Carrusel manual añadido

--- Client/src/Components/Carrousel/Carrousel.js
+++ Client/src/Components/Carrousel/Carrousel.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState, useEffect, useTransition } from "react";
 
-function Carrousel() {
-  return <div>Carrousel</div>;
-}
+const carrouselImages = [
+  {
+    image: "https://i.im.ge/2022/08/26/OmzHlr.1.png",
+    url: "https://www.page.com.ar",
+    alt: "Mi vecino totoro",
+  },
+  {
+    image: "https://i.im.ge/2022/08/26/Omzfwf.2.png",
+    url: "https://www.page.com.ar",
+    alt: "Kiki entregas a domicilio",
+  },
+  {
+    image: "https://i.im.ge/2022/08/26/OmzkKm.3.png",
+    url: "https://www.page.com.ar",
+    alt: "El mundo secreto de Arrietty",
+  },
+  {
+    image: "https://i.im.ge/2022/08/26/OmzpiW.4.png",
+    url: "https://www.page.com.ar",
+    alt: "Una voz silenciosa",
+  },
+];
+
+const Carrousel = (_carrouselImages) => {
+  const [current, setCurrent] = useState(0);
+  const length = carrouselImages.length;
+
+  const prevImg = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  const nextImg = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  // setInterval(() => {
+  //   nextImg();
+  // }, 1500);
+
+  return (
+    <div className="carrousel">
+      <button className="carrousel--left--arrow" onClick={prevImg}>
+        {"<"}
+      </button>
+      <button className="carrousel--right--arrow" onClick={nextImg}>
+        {">"}
+      </button>
+
+      {carrouselImages.map((carrouselImages, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <a href={carrouselImages.url}>
+                <img
+                  src={carrouselImages.image}
+                  alt={carrouselImages.alt}
+                  className="image"
+                />
+              </a>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Carrousel;
