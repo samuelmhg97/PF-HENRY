274504254cd31b8b9aa77558c9c1347773c0230f
samuelmhg97 <samuelmhg97@gmail.com>
2022-08-26 17:41:18 -0300

    Nav

--- Client/src/Components/SearchBar/SearchBar.jsx
+++ Client/src/Components/SearchBar/SearchBar.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "../SearchBar/SearchBar.css"
 function SearchBar() {
-
+  const categories = [{
+    image: "https://flyclipart.com/thumb2/mobile-menu-icon-png-40149.png",
+    alt: "Categorie"
+  }]
   const logo = [{
     image: "https://img.freepik.com/vector-premium/logo-cine_23-2147503279.jpg?1&w=740",
-    url: "https://www.page.com.ar",
     alt: "logo"
   }]
   const LoginLogo = [{
     image: "https://thumbs.dreamstime.com/b/user-icon-trendy-flat-style-isolated-grey-background-user-symbol-user-icon-trendy-flat-style-isolated-grey-background-123663211.jpg",
-    url: "https://www.page.com.ar",
     alt: "login"
   }]
   return (
   <div className="SearchBar-container">
-    <select>
+    <select className="SearchBar-menu">
+      <img src={categories[0].image} alt="" />
       <option className="">Categories</option>
       <option className="">Promotions</option>
       <option className="">Acerca De</option>
