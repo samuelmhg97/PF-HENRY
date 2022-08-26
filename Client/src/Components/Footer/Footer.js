50c3398cfbb6bb93fc22ed936e80ce1634f35af9
AlannV <97909677+AlannV@users.noreply.github.com>
2022-08-26 16:37:41 -0300

    Footer, social media y carrousel editados

--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer--container">
+      <div className="footer">
+        <NavLink className="footer--link" to={""}>
+          Terms {"&"} Conditions
+        </NavLink>
+        <NavLink className="footer--link" to={""}>
+          About Us
+        </NavLink>
+        <NavLink className="footer--link" to={""}>
+          Contact Us
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- Client/src/Components/SocialMedia/SocialMedia.js
+++ Client/src/Components/SocialMedia/SocialMedia.js
@@ -7,20 +7,26 @@ function SocialMedia() {
         <a href="https://www.twitter.com">
           <img
             src="https://pnggrid.com/wp-content/uploads/2021/07/Twitter-Logo-Square-150x150.png"
-            alt="twitter"
+            alt="Twitter"
           />
         </a>
       </div>
 
       <div className="socialMedia--facebook">
         <a href="https://www.facebook.com">
-          <img src="https://pnggrid.com/wp-content/uploads/2021/07/Facebook-Logo-Square-150x150.png"></img>
+          <img
+            alt="Facebook"
+            src="https://pnggrid.com/wp-content/uploads/2021/07/Facebook-Logo-Square-150x150.png"
+          ></img>
         </a>
       </div>
 
       <div className="socialMedia--instagram">
         <a href="https://www.instagram.com">
-          <img src="https://pnggrid.com/wp-content/uploads/2021/06/Instagram-Logo-Transparent-1-150x150.png"></img>
+          <img
+            alt="Instagram"
+            src="https://pnggrid.com/wp-content/uploads/2021/06/Instagram-Logo-Transparent-1-150x150.png"
+          ></img>
         </a>
       </div>
     </div>
