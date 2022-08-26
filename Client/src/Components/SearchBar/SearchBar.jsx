186893708da392fdb5a957850dc2d003911f8d9e
samuelmhg97 <samuelmhg97@gmail.com>
2022-08-26 16:55:55 -0300

    2d Nav Advance

--- Client/src/Components/SearchBar/SearchBar.jsx
+++ Client/src/Components/SearchBar/SearchBar.jsx
@@ -1,22 +1,42 @@
 import React from "react";
-
+import "../SearchBar/SearchBar.css"
 function SearchBar() {
+
+  const logo = [{
+    image: "https://img.freepik.com/vector-premium/logo-cine_23-2147503279.jpg?1&w=740",
+    url: "https://www.page.com.ar",
+    alt: "logo"
+  }]
+  const LoginLogo = [{
+    image: "https://thumbs.dreamstime.com/b/user-icon-trendy-flat-style-isolated-grey-background-user-symbol-user-icon-trendy-flat-style-isolated-grey-background-123663211.jpg",
+    url: "https://www.page.com.ar",
+    alt: "login"
+  }]
   return (
   <div className="SearchBar-container">
     <select>
-      <label className="">Categories</label>
-      <label className="">Promotions</label>
-      <label className="">Acerca De</label>
-      <label className="">Ayuda</label>
-      <label className="">Log In</label>
-      <label className="">Register</label>
+      <option className="">Categories</option>
+      <option className="">Promotions</option>
+      <option className="">Acerca De</option>
+      <option className="">Ayuda</option>
+      <option className="">Log In</option>
+      <option className="">Register</option>
     </select>
-    <img className="SearchBar-logo" alt="Logo"/>
+    {logo?.map((logo, key)=>{
+      return(
+        <img className="Searchlogo" src={logo.image} alt={logo.alt} key={key} />
+      )
+    })}
     <div>
       <input className="" type="text" placeholder="Search..."></input>
       <button className=""></button>
     </div>
-    <img className="" alt="LogIn-logo"/>
+    {/* <img className="" alt="LogIn-logo"/> */}
+    {LoginLogo?.map((LoginLogo, key)=>{
+      return(
+        <img className="Searchlogin" src={LoginLogo.image} alt={LoginLogo.alt} key={key} />
+      )
+    })}
   </div>
   );
 }
