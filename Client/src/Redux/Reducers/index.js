3ee6defcb6cb3ef12aef3934644411eff0257c15
AlannV <97909677+AlannV@users.noreply.github.com>
2022-08-29 11:34:41 -0300

    fixed errors

--- Client/src/Redux/Reducers/index.js
+++ Client/src/Redux/Reducers/index.js
@@ -3,29 +3,30 @@ const initialState = {
   movieDetail: {},
 };
 
-
-export default function rootReducer(state=initialState, action){
-  switch(action.type){
-    case 'GET_MOVIES': 
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_MOVIES":
       return {
         ...state,
-        movies: action.payload
-      }
-    case 'GET_MOVIE_DETAIL': 
+        movies: action.payload,
+      };
+    case "GET_MOVIE_DETAIL":
       return {
         ...state,
-        movieDetail: action.payload
-      }
-    case 'DEL_MOVIE_DETAIL': 
+        movieDetail: action.payload,
+      };
+    case "DEL_MOVIE_DETAIL":
       return {
         ...state,
-        movieDetail: {}
-      }
+        movieDetail: {},
+      };
     case "POST_MOVIE":
       return {
         ...state,
-      }
-    default: 
-    return {
-      ...state
-    }
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+}
