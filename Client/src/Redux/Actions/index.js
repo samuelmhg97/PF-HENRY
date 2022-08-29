74a7de93b1b8ed5037011f098e60f640e16e2a07
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-08-29 00:45:43 -0300

    Conectando el front con el back en billboard y moviedetail, un poco de responsive

--- Client/src/Redux/Actions/index.js
+++ Client/src/Redux/Actions/index.js
@@ -1,20 +1,25 @@
 const axios = require('axios');
-const apiKey = '4ef335cb';
 
-// export function getMovies(){
-//     return function(dispatch){
-//         return axios.get('')
-//         .then(r => r.json())
-//         .then(d => dispatch({type: 'GET_MOVIES', payload: d}))
-//         .catch(e => console.log(e));
-//     }
-// }
+export function getMovies(){
+    return function(dispatch){
+        return axios.get('http://api-pf-cine.herokuapp.com/movies')
+        .then(r => r.data)
+        .then(d => dispatch({type: 'GET_MOVIES', payload: d}))
+        .catch(e => console.log(e));
+    }
+}
 
 export function getMovieDetail(id){
     return function(dispatch){
-        return axios.get(`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+        return axios.get(`http://api-pf-cine.herokuapp.com/movies/${id}`)
         .then(r => r.data)
         .then(d => dispatch({type: 'GET_MOVIE_DETAIL', payload: d}))
         .catch(e => console.log(e));
+    }
+}
+
+export function delMovieDetail(){
+    return {
+        type: 'DEL_MOVIE_DETAIL'
     }
 }
--- Client/src/Redux/Reducers/index.js
+++ Client/src/Redux/Reducers/index.js
@@ -5,10 +5,20 @@ const initialState = {
 
 export default function rootReducer(state=initialState, action){
   switch(action.type){
+    case 'GET_MOVIES': 
+      return {
+        ...state,
+        movies: action.payload
+      }
     case 'GET_MOVIE_DETAIL': 
       return {
         ...state,
         movieDetail: action.payload
+      }
+    case 'DEL_MOVIE_DETAIL': 
+      return {
+        ...state,
+        movieDetail: {}
       }
     default: 
     return {
